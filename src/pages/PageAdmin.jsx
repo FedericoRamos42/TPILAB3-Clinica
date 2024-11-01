5a41d7a61fce6f1c5acc11242853356124a694a8
Federico Ramos <federicojramos42@gmail.com>
2024-10-31 21:46:48 -0300

    tabla admin fetch

--- src/pages/PageAdmin.jsx
+++ src/pages/PageAdmin.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import TableAdmin from '../components/Table/TableAdmin';
 
 const PageAdmin = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        const response = await fetch('http://localhost:5190/api/User');
+        console.log(response)
+        if (!response.ok) {
+          throw new Error("Error fetching Users");
+        }
+        const users = await response.json();
+        console.log(users);
+        setUsers(users);
+      } catch (error) {
+        console.error("Error fetching Users:", error);
+      }
+    };
+    fetchAppointments();
+  }, []);
+
+
   return (
-    <div>PageAdmin</div>
+    <TableAdmin users={users}/>
   );
 };
 
