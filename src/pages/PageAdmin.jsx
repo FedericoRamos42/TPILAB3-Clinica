088731310035012f20df289005ab9c69a6a1970f
agustinservin1 <agustinservin199103@gmail.com>
2024-11-01 21:18:42 -0300

    Filtrado tabla admin

--- src/pages/PageAdmin.jsx
+++ src/pages/PageAdmin.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import TableGeneric from '../components/Table/TableGeneric';
-import { headerAdmin} from '../data/headerTable';
+import { headerAdmin } from '../data/headerTable';
+import { ComboBoxGeneric } from '../components/ComboBox';
+
 // import TableAdmin from '../components/Table/TableAdmin';
 // import { AdminHeader } from '../data/AdminHeader';
 const PageAdmin = () => {
   const [users, setUsers] = useState([]);
+  const [stateFiltered, setStateFiltered] = useState(null);
 
   useEffect(() => {
     const fetchAppointments = async () => {
@@ -24,47 +27,108 @@ const PageAdmin = () => {
     fetchAppointments();
   }, []);
 
-  const handleDeleteAppointment = async (id) => {
-    console.log(id)
-    try {
-        const response = await fetch(`http://localhost:5190/api/User/Delete/${id}`, {
-            method: 'Delete',
-            headers: {
-                // 'Authorization': `Bearer ${user.token}`,
-                'Content-Type': 'application/json',
-            },
+
+  //     try {
+  //       const response = await fetch(`http://localhost:5190/api/User/${Id}`, {
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+
+  //       });
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error solicicitando usuario:", error);
+  //     }
+  //   };
+  //   fetchAdmins();
+  // }, []);
+
+
+  useEffect(() => {
+
+    const fetchAdmins = async () => {
+      const query = new URLSearchParams();
+      if (stateFiltered) query.append('state', stateFiltered);
+
+      try {
+        const response = await fetch(`http://localhost:5190/api/User/Filtered?${query.toString()}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
         });
 
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
-    } catch (error) {
+        const user = await response.json();
+        setUsers(user); // Actualizamos con el usuario encontrado
+        console.log("User fetched:", user);
+
+      } catch (error) {
+        console.error("Error solicitando usuario:", error);
+        
+      }
+      {
+      fetchAdmins();
+    }, [stateFiltered])
+    
+    
+
+    
+
+
+    const handleDeleteAppointment = async (id) => {
+      console.log(id)
+      try {
+        const response = await fetch(`http://localhost:5190/api/User/Delete/${id}`, {
+          method: 'Delete',
+          headers: {
+            // 'Authorization': `Bearer ${user.token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+      } catch (error) {
         console.error("Error eliminando un usuario:", error);
-    }
+      }
 
-};
+    };
 
-  const actions = users.map((user) => [
-    {
-      icon: 'edit',
-      color: 'primary',
-      onClick: () => handleEditAppointment(user.id),
-    },
-    {
-      icon: 'ban',
-      color: 'danger',
-      onClick: () => handleDeleteAppointment(user.id),
-    },
+    const actions = users.map((user) => [
+      {
+        icon: 'edit',
+        color: 'primary',
+        onClick: () => handleEditAppointment(user.id),
+      },
+      {
+        icon: 'ban',
+        color: 'danger',
+        onClick: () => handleDeleteAppointment(user.id),
+      },
 
-  ]);
+    ]);
 
-  return (
-    <>
-      <TableGeneric headers={headerAdmin} data={users} actions={actions} />
-    </>
-   
-  );
-};
+    return (
+      <>
+        <ComboBoxGeneric label="Estado"
+          options={[{ value: '', label: 'Todos los Estados' },
+          { value: 'Active', label: 'Activo' },
+          { value: 'Inactive', label: 'Inactivo' },]}
+          onSelect={setStateFiltered}
+        />
 
-export default PageAdmin;
+        <TableGeneric headers={headerAdmin} data={users} actions={actions} />
+      </>
+
+    );
+
+    export default PageAdmin;
